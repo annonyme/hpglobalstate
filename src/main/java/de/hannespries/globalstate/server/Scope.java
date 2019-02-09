079ba81528aa72d94a881e8582605d8bb7b87d52
Hannes Pries <annonyme@annonyme.de>
2019-02-09 19:45:54 +0100

    WIP: removed key in scope

--- src/main/java/de/hannespries/globalstate/server/Scope.java
+++ src/main/java/de/hannespries/globalstate/server/Scope.java
@@ -1,12 +1,7 @@
 package de.hannespries.globalstate.server;
 
 import de.hannespries.globalstate.GlobalState;
-import io.jsonwebtoken.SignatureAlgorithm;
-import io.jsonwebtoken.security.Keys;
-
-import java.security.Key;
 
 public class Scope {
     public static final GlobalState state = new GlobalState();
-    public static final Key key = Keys.secretKeyFor(SignatureAlgorithm.HS256);
 }
