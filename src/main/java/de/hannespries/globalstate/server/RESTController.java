41e1f4383833e5206201500e29ccd39d44f403d5
Hannes Pries <annonyme@annonyme.de>
2019-02-09 15:55:02 +0100

    WIP: improved controller produces json annotations

--- src/main/java/de/hannespries/globalstate/server/RESTController.java
+++ src/main/java/de/hannespries/globalstate/server/RESTController.java
@@ -2,22 +2,23 @@ package de.hannespries.globalstate.server;
 
 import de.hannespries.globalstate.Action;
 
-import javax.ws.rs.Consumes;
-import javax.ws.rs.GET;
-import javax.ws.rs.POST;
-import javax.ws.rs.Path;
+import javax.enterprise.context.ApplicationScoped;
+import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import java.util.Map;
 
 @Path("globalstate")
+@ApplicationScoped
 public class RESTController {
     @POST
     @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
     public Map<String, Object> action(Action action){
         return Scope.state.action(action);
     }
 
     @GET
+    @Produces(MediaType.APPLICATION_JSON)
     public String get(){
         return "{\"online\": true}";
     }
